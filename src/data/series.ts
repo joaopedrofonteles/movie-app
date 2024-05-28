cbb2f14ae4e3a201401c94bbb59a113c3bfbf694
joaopedrofonteles <dev.fonteles@gmail.com>
2024-05-27 22:13:45 -0300

    feat: add serie mock data and serie type

---
+++ src/data/series.ts
@@ -0,0 +1,63 @@
+import {Serie} from "../types/serie"
+
+export const series: Serie [] = [
+  {
+    id:"1", 
+    title: "Loren ipsum dolor",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ",
+    date: "08.08.2021",
+    cover: "/card-image.png",
+    badges: ["horror", "vecna"],
+  },
+  {
+    id:"2", 
+    title: "Loren ipsum dolor",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ",
+    date: "08.08.2021",
+    cover: "/card-image.png",
+    badges: ["horror", "vecna"],
+  } , {
+    id:"3", 
+    title: "Loren ipsum dolor",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ",
+    date: "08.08.2021",
+    cover: "/card-image.png",
+    badges: ["horror", "vecna"],
+  } , {
+    id:"4", 
+    title: "Loren ipsum dolor",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ",
+    date: "08.08.2021",
+    cover: "/card-image.png",
+    badges: ["adventure", "vecna"],
+  } , {
+    id:"5", 
+    title: "Loren ipsum dolor",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ",
+    date: "08.08.2021",
+    cover: "/card-image.png",
+    badges: ["adventure", "vecna"],
+  } , {
+    id:"6", 
+    title: "Loren ipsum dolor",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ",
+    date: "08.08.2021",
+    cover: "/card-image.png",
+    badges: ["adventure", "vecna"],
+  } , {
+    id:"7", 
+    title: "Loren ipsum dolor",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ",
+    date: "08.08.2021",
+    cover: "/card-image.png",
+    badges: ["adventure", "vecna"],
+  },
+  {
+    id:"8", 
+    title: "Loren ipsum dolor",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ",
+    date: "08.08.2021",
+    cover: "/card-image.png",
+    badges: ["adventure", "vecna"],
+  }
+]
---
+++ src/types/serie.ts
@@ -0,0 +1,8 @@
+export interface Serie {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+  cover: string;
+  badges?: string[];
+}
